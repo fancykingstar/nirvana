7509acd675aa27f2d28e10ae2b10bb7ccd7d8866
Freddie Ridell <contact@freddieridell.com>
2020-10-18 13:53:14 +0100

    added Breadcrumb

--- src/component/Main/index.js
+++ src/component/Main/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import FormEditCity from "../FormEditCity";
+import Breadcrumb from "../Breadcrumb";
 import FilterListCity from "../FilterListCity";
+import FormEditCity from "../FormEditCity";
 
 export default function Main() {
     return (
         <main>
+            <Breadcrumb />
             <Switch>
                 <Route path="/:env/city/edit/:id" component={FormEditCity} />
-                <Route path="/:env/city/list" component={FilterListCity} />
+                <Route path="/:env/city" component={FilterListCity} />
             </Switch>
         </main>
     );
