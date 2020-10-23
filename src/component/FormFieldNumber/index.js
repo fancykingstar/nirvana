3e23bc6cccc8a9acf620400b96447875a40da1a3
Christopher Martin <chris.martin@imaginecruising.co.uk>
2020-10-23 15:58:49 +0100

    Added form number field component

---
+++ src/component/FormFieldNumber/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+import { useFormField } from "../../hooks/useFormContext";
+
+import FormFieldLabel from "../FormFieldLabel";
+
+const FieldChanged = styled.span`
+    grid-column: updated / updated;
+`;
+
+const FieldInput = styled.input`
+    padding: ${(p) => p.theme.size[0]};
+    border-radius: ${(p) => p.theme.size[0]};
+    font-size: ${(p) => p.theme.text[2]};
+    grid-column: input / input;
+`;
+
+export default function FormFieldNumber({ required, prop, label }) {
+    const [state, setState, changed] = useFormField(prop, "");
+
+    return (
+        <React.Fragment>
+            <FormFieldLabel required={required}>{label}</FormFieldLabel>
+            <FieldInput
+                type="number"
+                value={state}
+                onChange={(e) => setState(e.target.value)}
+            />
+
+            {changed ? <FieldChanged>updated</FieldChanged> : null}
+        </React.Fragment>
+    );
+}
