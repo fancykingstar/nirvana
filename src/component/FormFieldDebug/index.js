1172d6d250ddb5551ceb54c9309e9b7e5a0fd49f
Freddie Ridell <contact@freddieridell.com>
2020-10-30 16:48:03 +0000

    fixed lint issue

--- src/component/FormFieldDebug/index.js
+++ src/component/FormFieldDebug/index.js
@@ -2,14 +2,12 @@ import React from "react";
 
 import { FormContext } from "../../hooks/useFormContext";
 
-import FormFieldLabel from "../FormFieldLabel";
-
 export default function FormFieldDebug() {
     const {
         state: { local },
     } = React.useContext(FormContext);
 
-    //console.log(local);
+    console.log(local);
 
     return null;
 }
